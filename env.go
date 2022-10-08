b5107d8e5f250aea789f67fa6bf2c6866dee14a2
Ruben van Staden <ruben@anyonsys.com>
2022-10-07 20:25:21 -0400

    feat: add http and grpc regex

---
+++ env.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+	"regexp"
+)
+
+func RpcAddr(key string) string {
+
+    value, ok := os.LookupEnv(key)
+	if !ok {
+        log.Fatalf("address env variable \"%s\" not set, usual", key)
+	}
+
+	re := regexp.MustCompile(ipAddress())
+    match := re.MatchString(value)
+
+    if !match {
+        log.Fatalf("invalid HTTP address env variable \"%s\"", key)
+    }
+
+	return value
+}
+
+func HttpAddr(key string) string {
+
+    value, ok := os.LookupEnv(key)
+	if !ok {
+        log.Fatalf("address env variable \"%s\" not set, usual", key)
+	}
+
+    re := regexp.MustCompile(`http://` + ipAddress())
+    match := re.MatchString(value)
+
+    if !match {
+        log.Fatalf("invalid HTTP address env variable \"%s\"", key)
+    }
+
+	return value
+}
+
+func Test(key string) string {
+
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return ""
+}
--- main.go
+++ main.go
@@ -2,52 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"regexp"
 )
 
-func HasValue(key string) bool {
-	if _, ok := os.LookupEnv(key); ok {
-		return true
-	}
-	return false
-}
-
-func RpcAddr(key string) string {
-
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-
-	log.Fatalf("address env variable \"%s\" not set, usual", key)
-
-	return ""
-}
-
-func HttpAddr(key string) string {
-
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-
-	log.Fatalf("address env variable \"%s\" not set, usual", key)
-
-	return ""
-}
-
-func Test(key string) string {
-
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-
-	return ""
+func ipAddress() string {
+    domain := `(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})`
+    port := `:(\d{4})`
+    return domain + port
 }
 
 func main() {
-
-	match, _ := regexp.MatchString(`(\d{1,3})\.(\d)\.(\d)\.(\d):(\d{1,4})`, "123450.0.0.0:8080")
+	re := regexp.MustCompile(ipAddress())
+    match := re.MatchString("http://000.12.12.034:0000")
 	fmt.Println(match)
-
 }
